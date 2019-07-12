16549f884bac3cce5dcdc0f7ec0113f10236aeb1
Yevseyenko <jevsejen@gmail.com>
2019-07-13 00:26:57 +0300

    delegate

---
+++ src/main/java/com/patterns/Delegate.java
@@ -0,0 +1,66 @@
+package com.patterns;
+
+public class Delegate {
+    public static void main(String[] args) {
+        A a = new A();
+        a.f();
+        Painter painter = new Painter();
+        painter.setGraphics(new Square());
+        painter.draw();
+        painter.setGraphics(new Triangle());
+        painter.draw();
+        painter.setGraphics(new Circle());
+        painter.draw();
+    }
+}
+
+class A {
+    void f() {
+        System.out.println("f()");
+    }
+}
+
+class B {
+    A a = new A();
+
+    void f() {
+        a.f();
+    }
+}
+
+interface Graphics {
+    void draw();
+}
+
+class Triangle implements Graphics {
+    @Override
+    public void draw() {
+        System.out.println("Drawing triangle");
+    }
+}
+
+class Square implements Graphics {
+    @Override
+    public void draw() {
+        System.out.println("Drawing square");
+    }
+}
+
+class Circle implements Graphics {
+    @Override
+    public void draw() {
+        System.out.println("Drawing circle");
+    }
+}
+
+class Painter {
+    Graphics graphics;
+
+    void setGraphics(Graphics g) {
+        graphics = g;
+    }
+
+    void draw() {
+        graphics.draw();
+    }
+}
